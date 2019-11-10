f4e05f66f98429b88e65852a02aa758946de9efa
Mandeep Vakani <mvakani@yahoo.com>
2019-11-10 20:05:20 +0530

    with Bird Image Classifier using FastAI PyTourch

--- app.py
+++ app.py
@@ -1,23 +1,49 @@
+import mimetypes
+import os
+from io import BytesIO
+from typing import List, Dict, Union, ByteString, Any
+from urllib.parse import urlparse
+
 from flask import Flask, request
+import requests
 from twilio.twiml.messaging_response import MessagingResponse
+
+from fastai.vision import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
-@app.route("/whatsapp", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
+model = load_learner('.')
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+def load_image(raw_bytes: ByteString) -> Image:
+    img = open_image(BytesIO(raw_bytes))
+    return img
+	
 
-    return str(resp)
+@app.route("/whatsapp", methods=["GET", "POST"])
+def reply_whatsapp():
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    num_media = int(request.values.get("NumMedia"))
+    media_files = []
+    for idx in range(num_media):
+        media_url = request.values.get(f'MediaUrl{idx}')
+        mime_type = request.values.get(f'MediaContentType{idx}')
+        media_files.append((media_url, mime_type))
+
+        req = requests.get(media_url)
+        file_extension = mimetypes.guess_extension(mime_type)
+        media_sid = os.path.basename(urlparse(media_url).path)
+
+ #       with open(f"app_data/{media_sid}{file_extension}", 'wb') as f:
+ #           f.write(req.content)
+        img = load_image(req.content)
+        pred_class, pred_idx, outputs = model.predict(img)
+	
+    response = MessagingResponse()
+
+    if not num_media:
+        msg = response.message("Please, send us a Bird image!")
+    else:
+        msg = response.message("Thanks for the "+ str(pred_class)+" image!")
+
+    return str(response)
